c2066aa8241bb3aca4d9451954599644614c55d0
Daniel Kim <70085359+dkim0722@users.noreply.github.com>
2026-02-13 10:23:02 -0800

    Hid SteeringDemo unfinished project

--- SteeringDemo/Debug/Drivers/ugv_drivers/adc/ugv_adc.d
+++
@@ -1,3 +0,0 @@
-Drivers/ugv_drivers/adc/ugv_adc.o: ../Drivers/ugv_drivers/adc/ugv_adc.c \
- ../Drivers/ugv_drivers/adc/ugv_adc.h
-../Drivers/ugv_drivers/adc/ugv_adc.h:
--- SteeringDemo/Debug/Drivers/ugv_drivers/pid/ugv_pid.d
+++
@@ -1,3 +0,0 @@
-Drivers/ugv_drivers/pid/ugv_pid.o: ../Drivers/ugv_drivers/pid/ugv_pid.c \
- ../Drivers/ugv_drivers/pid/ugv_pid.h
-../Drivers/ugv_drivers/pid/ugv_pid.h:
